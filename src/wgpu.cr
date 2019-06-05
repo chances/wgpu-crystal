c552f87f51568b45964fecb54c4050fb97ee3cd1
Chance Snow <git@chancesnow.me>
2019-06-05 05:20:21 -0700

    Add color constants

--- src/wgpu.cr
+++ src/wgpu.cr
@@ -16,3 +16,12 @@ module WGPU
     end
   end
 end
+
+module WGPU::Colors
+  TRANSPARENT = WGPUColor.new r: 0, g: 0, b: 0, a: 0
+  BLACK       = WGPUColor.new r: 0, g: 0, b: 0, a: 1
+  WHITE       = WGPUColor.new r: 1, g: 1, b: 1, a: 1
+  RED         = WGPUColor.new r: 1, g: 0, b: 0, a: 1
+  GREEN       = WGPUColor.new r: 0, g: 1, b: 0, a: 1
+  BLUE        = WGPUColor.new r: 0, g: 0, b: 1, a: 1
+end
