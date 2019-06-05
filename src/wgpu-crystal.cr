07f2457fc8ee9c1ad12e2a82367d14ddb9dd6b8b
Chance Snow <git@chancesnow.me>
2019-06-05 03:48:51 -0700

    Rename shard lib to plainly "wgpu"

--- src/wgpu-crystal.cr
+++
@@ -1,6 +0,0 @@
-# TODO: Write documentation for `Wgpu::Crystal`
-module Wgpu::Crystal
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
-end
---
+++ src/wgpu.cr
@@ -0,0 +1,10 @@
+# TODO: Write documentation for `Wgpu`
+@[Link(ldflags: "-L#{__DIR__}/../vendor/wgpu/target/debug -lwgpu_native")]
+lib LibWGPU
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
+
+module WGPU
+end
