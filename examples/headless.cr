aeff0d994562bc86d7da76f0610aa4b5ba50bc61
Chance Snow <git@chancesnow.me>
2020-10-23 03:51:40 -0500

    Add `create_buffer` and `create_texture` methods to Device class wrapper

--- examples/headless.cr
+++ examples/headless.cr
@@ -4,8 +4,37 @@ puts "Headless WebGPU Instance"
 
 Signal::INT.trap { exit }
 
-instance = WGPU::Instance.new
-pp instance.id
+adapter = WGPU::Adapter.new(LibWGPU::RequestAdapterOptions.new power_preference: LibWGPU::PowerPreference::LowPower)
+device = WGPU::Device.new adapter
 
-adapter = WGPU::Adapter.new(instance, LibWGPU::WGPUAdapterDescriptor.new power_preference: LibWGPU::WGPUPowerPreference::LowPower)
-pp adapter.id
+width : UInt64 = 300
+height : UInt64 = 400
+
+# https://github.com/rukai/wgpu-rs/blob/f6123e4fe89f74754093c07b476099623b76dd08/examples/capture/main.rs#L53
+bytes_per_pixel = sizeof(UInt32);
+unpadded_bytes_per_row = width * bytes_per_pixel;
+align = WGPU::COPY_BYTES_PER_ROW_ALIGNMENT;
+padded_bytes_per_row_padding = (align - unpadded_bytes_per_row % align) % align;
+padded_bytes_per_row = unpadded_bytes_per_row + padded_bytes_per_row_padding;
+
+# The output buffer lets us retrieve data as an array
+output_buffer = device.create_buffer(LibWGPU::BufferDescriptor.new(
+  label: nil,
+  size: padded_bytes_per_row * height,
+  usage: WGPU::BufferUsage::MapRead | WGPU::BufferUsage::CopyDst,
+  mapped_at_creation: false
+))
+texture_extent = LibWGPU::Extent3d.new width: width, height: height, depth: 1
+
+# The render pipeline renders data into this texture
+texture = device.create_texture(LibWGPU::TextureDescriptor.new(
+  size: texture_extent,
+  mip_level_count: 1,
+  sample_count: 1,
+  dimension: LibWGPU::TextureDimension::D2,
+  format: LibWGPU::TextureFormat::Rgba8UnormSrgb,
+  usage: WGPU::TextureUsage::OutputAttachment | WGPU::TextureUsage::CopySrc,
+  label: nil
+))
+
+# TODO: Set the background to be red
--- src/wgpu.cr
+++ src/wgpu.cr
@@ -4,64 +4,120 @@ require "./lib-wgpu"
 module WGPU
   VERSION = "0.6.0"
 
-  class Instance
-    def initialize
-      @instanceId = LibWGPU.wgpu_create_instance
-    end
+  COPY_BYTES_PER_ROW_ALIGNMENT = LibWGPU::COPY_BYTES_PER_ROW_ALIGNMENT
 
-    def id
-      @instanceId
-    end
+  enum ShaderStage : UInt8
+    None = 0
+    Vertex = 1
+    Fragment = 2
+    Compute = 4
+  end
+  enum BufferUsage : UInt16
+    MapRead = 1
+    MapWrite = 2
+    CopySrc = 4
+    CopyDst = 8
+    Index = 16
+    Vertex = 32
+    Uniform = 64
+    Storage = 128
+    Indirect = 256
+  end
+  enum TextureUsage : UInt8
+    CopySrc = 1
+    CopyDst = 2
+    Sampled = 4
+    Storage = 8
+    OutputAttachment = 16
   end
 
   class Adapter
-    def initialize(instance : Instance, adapterDescriptor : LibWGPU::WGPUAdapterDescriptor)
-      unless instance
-        raise ArgumentError.new(message = "instance must not be nil")
-      else
-        @adapterId = LibWGPU.wgpu_instance_get_adapter(instance.id, pointerof(adapterDescriptor))
-      end
+    @id : LibWGPU::AdapterId = 0
+    @@callback_box : Pointer(Void)?
+
+    def initialize(options : LibWGPU::RequestAdapterOptions)
+      callback = ->(adapter_id : LibWGPU::AdapterId) { @id = adapter_id }
+      callback_boxed = Box.box(callback)
+      @@callback_box = callback_boxed
+
+      allowed_backends = LibWGPU::Backend::Vulkan | LibWGPU::Backend::Metal | LibWGPU::Backend::Dx12
+      LibWGPU.wgpu_request_adapter_async(pointerof(options), allowed_backends, false, ->(adapter_id, data) {
+        cb = Box(typeof(callback)).unbox(data)
+        cb.call(adapter_id)
+      }, callback_boxed)
+    end
+
+    def is_ready?
+      @id > 0
     end
 
     def id
-      @adapterId
+      @id
+    end
+
+    def to_unsafe
+      @id
     end
   end
 
   class Device
-    def initialize(adapter : Adapter, deviceDescriptor : LibWGPU::WGPUDeviceDescriptor)
-      @deviceId = LibWGPU.wgpu_adapter_request_device(adapter.id, pointerof(deviceDescriptor))
+    def initialize(adapter : Adapter)
+      unless adapter && adapter.is_ready?
+        raise ArgumentError.new(message = "adapter must be ready")
+      end
+      limits = LibWGPU::CLimits.new max_bind_groups: 1
+      @id = LibWGPU.wgpu_adapter_request_device(adapter, 0, pointerof(limits), true, nil)
     end
 
     def id
-      @deviceId
+      @id
+    end
+
+    def to_unsafe
+      @id
+    end
+
+    def create_buffer(descriptor : LibWGPU::BufferDescriptor)
+      LibWGPU.wgpu_device_create_buffer(self, pointerof(descriptor))
+    end
+
+    def create_texture(descriptor : LibWGPU::TextureDescriptor)
+      LibWGPU.wgpu_device_create_texture(self, pointerof(descriptor))
     end
   end
 
   class Surface
-    @@surfaceId : LibWGPU::WGPUSurfaceId?
+    @@id : LibWGPU::SurfaceId?
 
     def id
-      @@surfaceId
+      @@id
+    end
+
+    def to_unsafe
+      @id
     end
   end
 
   class SwapChain
-    def initialize(device : Device, surface : Surface, swapChainDescriptor : LibWGPU::WGPUSwapChainDescriptor)
+    def initialize(device : Device, surface : Surface, swapChainDescriptor : LibWGPU::SwapChainDescriptor)
       @swapchainId = LibWGPU.wgpu_device_create_swap_chain(device.id, surface.id, pointerof(swapChainDescriptor))
     end
 
     def id
       @swapchainId
     end
+
+    def to_unsafe
+      @id
+    end
   end
 end
 
 module WGPU::Colors
-  TRANSPARENT = WGPUColor.new r: 0, g: 0, b: 0, a: 0
-  BLACK       = WGPUColor.new r: 0, g: 0, b: 0, a: 1
-  WHITE       = WGPUColor.new r: 1, g: 1, b: 1, a: 1
-  RED         = WGPUColor.new r: 1, g: 0, b: 0, a: 1
-  GREEN       = WGPUColor.new r: 0, g: 1, b: 0, a: 1
-  BLUE        = WGPUColor.new r: 0, g: 0, b: 1, a: 1
+  TRANSPARENT = LibWGPU::Color.new r: 0, g: 0, b: 0, a: 0
+  BLACK       = LibWGPU::Color.new r: 0, g: 0, b: 0, a: 1
+  WHITE       = LibWGPU::Color.new r: 1, g: 1, b: 1, a: 1
+  RED         = LibWGPU::Color.new r: 1, g: 0, b: 0, a: 1
+  GREEN       = LibWGPU::Color.new r: 0, g: 1, b: 0, a: 1
+  BLUE        = LibWGPU::Color.new r: 0, g: 0, b: 1, a: 1
 end
